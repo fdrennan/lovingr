fd3998ca3e0bdae2e92031f932c4df3cefd7b527
Freddy <drennanfreddy@gmail.com>
2022-09-12 12:50:47 -0600

    stored and shit

--- box/utilities/chatty/chatty.r
+++ box/utilities/chatty/chatty.r
@@ -3,6 +3,11 @@ chatty <- function(input) {
   box::use(jsonlite, shiny)
   json_data <- jsonlite$toJSON(shiny$reactiveValuesToList(input), pretty = TRUE)
   shiny$showNotification(
-    json_data
+    shiny$fluidRow(
+      shiny$column(
+        12,
+        shiny$pre(json_data)
+      )
+    )
   )
 }
